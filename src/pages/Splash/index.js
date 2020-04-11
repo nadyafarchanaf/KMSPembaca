7d527c5088dca726833f2ffbaba4f3528e69a41e
nadyafarchanaf <nadya_farchana@apps.ipb.ac.id>
2020-04-11 23:14:10 +0700

    first commit

---
+++ src/pages/Splash/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Splash = () => {
+    return (
+        <View>
+            <Text style={{color:'black'}}>Splash Screen</Text>
+        </View>
+    );
+};
+
+export default Splash;

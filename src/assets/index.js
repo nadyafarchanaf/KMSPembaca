a68c2c2b1201bc6c03099bfe6273f4a40c16cfbd
nadyafarchanaf <nadya_farchana@apps.ipb.ac.id>
2020-04-17 01:04:31 +0700

    konten feature dan icon

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import Avatar from './icon/Avatar.png';
+import Bell from './icon/Bell.png';
+import WelcomePage3 from './icon/campaign.png';
+import Logo from './icon/coconut-tree.png';
+import Ebook from './icon/e-book.png';
+import Petani from './icon/farmer.png';
+import Welcomepage2 from './icon/networking.png';
+import Artikel from './icon/artikel.png';
+import PakarFemale from './icon/teacher-female.png';
+import PakarMale from './icon/teacher-male.png';
+import Video from './icon/video.png';
+
+export {Avatar, Bell, Welcomepage2,WelcomePage3, Logo, Ebook, PakarFemale, PakarMale, Petani, Artikel, Video};
--- src/component/molecules/KontenFeature/index.js
+++ src/component/molecules/KontenFeature/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, Image, View}  from 'react-native';
+import { Avatar } from '../../../assets';
 
 const KontenFeature = ({title,img}) => {
     return (
         <View>
-            <Image source={img}/>
+            <Image source={Avatar}/>
             <Text> {title}</Text>
         </View>
     )
